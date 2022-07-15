bb10f1cafd97ab47b65be45bae15ae3e5002bda5
ZhaoYuLeo <zhaoyuleoo@gmail.com>
2022-07-15 15:03:26 +0800

    feat: add recursive version of hailstone which prints out values of the sequence and returns the number of steps

--- lab/disc03/disc03.py
+++ lab/disc03/disc03.py
@@ -16,3 +16,33 @@ def multiply(m, n):
         return multiply_helper(m, n)
     else:
         return multiply_helper(n, m)
+
+def hailstone(n):
+    """Print out the hailstone sequence starting at n, and return the number of elements in the sequence.
+    >>> a = hailstone(10)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    >>> a
+    7
+    """
+    def hailstone_helper(n, count = 1):
+        print(n)
+        if n == 1:
+            return count
+        if n % 2 == 0:
+            return hailstone_helper(n // 2, count + 1)
+        else:
+            return hailstone_helper(3 * n + 1, count + 1)
+    return hailstone_helper(n)
+    # print(n)
+    # if n == 1:
+    #     return 1 
+    # if n % 2 == 0:
+    #     return 1 + hailstone(n // 2)
+    # else:
+    #     return 1 + hailstone(3 * n + 1)
