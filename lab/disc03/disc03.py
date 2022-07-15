f1c40aa4b5fee9fb9b9b5f413bb857c057794098
ZhaoYuLeo <zhaoyuleoo@gmail.com>
2022-07-15 16:46:54 +0800

    feat: add merge which merges two numbers with digits in decreasing order

--- lab/disc03/disc03.py
+++ lab/disc03/disc03.py
@@ -17,6 +17,7 @@ def multiply(m, n):
     else:
         return multiply_helper(n, m)
 
+
 def hailstone(n):
     """Print out the hailstone sequence starting at n, and return the number of elements in the sequence.
     >>> a = hailstone(10)
@@ -30,6 +31,7 @@ def hailstone(n):
     >>> a
     7
     """
+    assert isinstance(n, int) and n > 0, "Argument of hailstone must be positive integer."
     def hailstone_helper(n, count = 1):
         print(n)
         if n == 1:
@@ -39,6 +41,7 @@ def hailstone(n):
         else:
             return hailstone_helper(3 * n + 1, count + 1)
     return hailstone_helper(n)
+    # # non tail recursive version
     # print(n)
     # if n == 1:
     #     return 1 
@@ -46,3 +49,42 @@ def hailstone(n):
     #     return 1 + hailstone(n // 2)
     # else:
     #     return 1 + hailstone(3 * n + 1)
+
+
+
+def merge(n1, n2):
+    """ Merges two numbers with digits in decreasing order and returns a single number with all of the digits of the two, in decreasing order.
+    >>> merge(31, 42)
+    4321
+    >>> merge(21, 0)
+    21
+    >>> merge (21, 31)
+    3211
+    """
+    assert n1 >= 0 and n2 >= 0, "Assume two arguments are both positive"
+    assert isinstance(n1, int) and isinstance(n2, int), "Assume two arguments are both integers"
+    def merge_helper(n1, n2, result = 0, digit = 0):
+        if n1 == 0 and n2 == 0:
+            return result
+        if n1 == 0:
+            return n2 * 10 ** digit + result
+        if n2 == 0:
+            return n1 * 10 ** digit + result
+        rem1, rem2 = n1 % 10, n2 % 10
+        if rem1 < rem2:
+            return merge_helper(n1 // 10, n2, rem1 * 10 ** digit + result, digit + 1)
+        else:
+            return merge_helper(n1, n2 // 10, rem2 * 10 ** digit + result, digit + 1)
+    return merge_helper(n1, n2)
+   # # non tail recursive version
+    # if n1 == 0 and n2 == 0:
+    #     return 0
+    # if n1 == 0:
+    #     return n2
+    # if n2 == 0:
+    #     return n1
+    # rem1, rem2 = n1 % 10, n2 % 10
+    # if rem1 < rem2:
+    #     return rem1 + 10 * merge(n1 // 10, n2) 
+    # else:
+    #     return rem2 + 10 * merge(n1, n2 // 10)
