cb76162d69850d5936ca4e4f9ed20877553a94fe
ZhaoYuLeo <zhaoyuleoo@gmail.com>
2022-07-19 23:44:28 +0800

    feat: add function remove which removes digit n(0...9) from a non-negative integer

--- lab/disc03/disc03.py
+++ lab/disc03/disc03.py
@@ -128,3 +128,19 @@ def is_prime(n):
         else:
             return prime_helper(count + 1)
     return prime_helper()
+
+
+def remove(n, digit):
+    """Return all digit of non-negative N that are not DIGIT, for some non-negative DIGIT less tahn 10.
+    >>> remove(231, 3)
+    21
+    >>> remove(243132, 2)
+    4313
+    """
+    kept, digits = 0, 0
+    while n > 0:
+        n, last = n // 10, n % 10
+        if last != digit:
+            kept = kept + last * 10 ** digits 
+            digits = digits + 1 
+    return kept 
