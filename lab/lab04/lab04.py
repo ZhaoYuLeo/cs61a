dd84049d206a935f3b9a4a4514c62fab9ef97373
ZhaoYuLeo <zhaoyuleoo@gmail.com>
2022-07-21 01:22:38 +0800

    feat: add function add_chars which removes w1 from w2 in order

--- lab/lab04/lab04.py
+++ lab/lab04/lab04.py
@@ -162,4 +162,10 @@ def add_chars(w1, w2):
     True
     """
     "*** YOUR CODE HERE ***"
+    if w1 == '':
+        return w2
+    if w1[0] == w2[0]:
+        return add_chars(w1[1:], w2[1:])
+    else:
+        return w2[0] + add_chars(w1, w2[1:])
 
