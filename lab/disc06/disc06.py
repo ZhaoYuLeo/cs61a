e52383ab38d2db7f5e391e16db42ddadd0d8babe
ZhaoYuLeo <zhaoyuleoo@gmail.com>
2022-09-30 04:21:15 +0800

    feat: discussions about iterators and generators

--- lab/disc06/disc06.py
+++ lab/disc06/disc06.py
@@ -105,3 +105,86 @@ def add_this_many(x, el, s):
         if v == x:
             times += 1
     return s.extend([el for i in range(0, times)])
+
+
+# Iterators
+
+# Calling the iter function on an iterable will create an iterator over that iterable. Each iterator keeps track of its position within the iterable. Calling the next function on an iterator will give the current value in the iterable and move the iterator's position to the next value.
+# The relationship between an iterable and an iterator is analogous to the relationship between a book and a bookmark - an iterable contains the data that is being iterated over, and an iterator keeps track of your position within that data.
+# Once an iterator has returned all the values in an iterable, subsequent calls to next on that iterable will result in a StopIteration exception. In order to be able to access the values in the iterable a second time, you would have to create a second iterator.
+# Most iterators are alse iterables - that is, calling iter on them will return an iterator. This means that we can use them inside for loops. However, calling iter on most iterators will not create a new iterator - instead, it will simply return the same iterator.
+
+"""
+>>> s = [[1, 2]]
+>>> i = iter(s)
+>>> j = iter(next(i))
+>>> next(j)
+1
+>>> s.append(3)
+>>> next(i)
+3
+>>> next(j)
+2
+>>> next(i)
+>>> next(i)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+StopIteration
+>>> try:
+        next(i)
+    except StopIteration:
+        print('No more values')
+No more values
+"""
+
+
+# Generators
+
+# When a generator function(including a yield statement) is called, it returns a generator object, which is a type of iterator(has a next method). A generator function is a special kind of Python function that uses a yield statement instead of a return statement to report values.
+# The yield statement is similar to a return statement. However, while a return statement closes the current frame after the function exits, a yield statement causes the frame to be saved until the next time next is called, which allows the generator to automatically keep track of the iteration state.
+# Once next is called again, execution resumes where it last stopped and continues until the next yield statement or the end of the function. A generator function can have multiple yield statements.
+# When yield from is called on an iterator, it will yield every value from that iterator.
+
+def filter(iterable, fn): 
+    """ A generator function only yields elements of iterable for which fn returns True
+    >>> is_even = lambda x: x % 2 == 0
+    >>> list(filter(range(5), is_even)) # a list of the values yielded from the call to filter
+    [0, 2, 4]
+    >>> all_odd = (2*y-1 for y in range(5))
+    >>> list(filter(all_odd, is_even))
+    []
+    >>> naturals = (n for n in range(1, 100))
+    >>> s = filter(naturals, is_even)
+    >>> next(s)
+    2
+    >>> next(s)
+    4
+    """
+    for e in iterable:
+        if fn(e):
+            yield e
+
+
+def merge(a, b): 
+    """ A generator function takes in two infinite generators a and b that are in increasing order without duplicates and returns a generator that has all the elements of both generators, in increasing order, without duplicates.
+    >>> def sequence(start, step):
+    ...     while True:
+    ...         yield start
+    ...         start += step
+    >>> a = sequence(2, 3) # 2, 5, 8, 11, 14, ...
+    >>> b = sequence(3, 2) # 3, 5, 7, 9, 11, 13, 15, ...
+    >>> result = merge(a, b) # 2, 3, 5, 7, 8, 9, 11, 13, 14, 15
+    >>> [next(result) for _ in range(10)]
+    [2, 3, 5, 7, 8, 9, 11, 13, 14, 15]
+    """
+    item_a, item_b = next(a), next(b)
+    while True:
+        if item_a < item_b:
+            yield item_a
+            item_a = next(a)
+        elif item_a == item_b:
+            yield item_a
+            item_a, item_b = next(a), next(b)
+        else:
+            yield item_b
+            item_b = next(b)
