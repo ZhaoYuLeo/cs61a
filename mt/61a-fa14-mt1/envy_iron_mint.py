52d62cd70f25515bb5adcb9b3d2d7c55abe37256
ZhaoYuLeo <zhaoyuleoo@gmail.com>
2023-07-01 20:01:56 +0800

    feat add fa14mt1q2

--- mt/61a-fa14-mt1/envy_iron_mint.py
+++ mt/61a-fa14-mt1/envy_iron_mint.py
@@ -11,13 +11,13 @@ peace(joy)
 
 """
 Global frame
-peace   func peace(today) [parent=Global]
-joy     func joy(peace) [parent=Global]
+peace   -> func peace(today) [parent=Global]
+joy     -> func joy(peace) [parent=Global]
 love    3
 harmony 2
 
 f1: peace [parent=Global]
-today   point to joy in global frame
+today   -> points to the joy in global frame
 harmony 5
 Return Value 7
 
@@ -25,4 +25,35 @@ f2: joy [parent=Global]
 peace   6
 love    5
 Return Value 2 #important, harmony == 2, where joy defined.
+"""
+
+def k(g, b):
+    def n(s, a):
+        return g-p
+    return b(n(b, p))
+
+g, p = 3, 7
+k(p+1, lambda s: g+3)
+
+"""
+Global frame
+k       ->  func k(g, b) [parent=Global]
+g       3
+p       7
+            func λ(s) <line 36> [parent=Global] 
+
+f1: k [parent=Global]
+g       8
+b       ->  points to λ <line 36> in global frame
+n       ->  func n(s, a) [parent=f1]
+Return Value    6
+
+f3: n [parent=f1] 
+s       ->  points to λ <line 36> in global frame
+a       7   #attention, we found p in global frame, g in f1 frame.
+Return Value    1
+
+f2: λ <line 36> [parent=Global]
+s       1
+Return Value    6 #attention, we found g in global frame.
 """
